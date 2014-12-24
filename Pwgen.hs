8d6a2ed49ee93cf21f9934ebdbcd12401667a1f5
meris <meris.bahti@gmail.com>
2014-12-24 12:26:49 +0100

    use a join-like operation to construct the password

--- Pwgen.hs
+++ Pwgen.hs
@@ -13,16 +13,22 @@ pick i xs = (pick' xs) : (pick (i-1) xs)
     pick' :: [a] -> IO a
     pick' xs = randomRIO (0, length xs - 1) >>= return . (xs !!)
  
-conts = do 
+files = do 
   fs <- getDirectoryContents "/usr/share/dict"
   return . (fmap ("/usr/share/dict/"++)) . filter (liftA2 (&&) (/=".") (/="..")) $ fs
 
+addMon :: IO String -> IO String -> IO String
+addMon x y = x >>= (first y)
+  where 
+    first :: IO String -> String -> IO String
+    first y x = y >>= (\t -> return (x ++ ' ':t))
+
+
 main = do
-  x <- conts >>= mapM readFile 
+  x <- files >>= mapM readFile 
   let dict = filter (liftA2 (&&) ((< upper).length) ((> lower).length)) (x >>= words)
-  let dictLen = fromIntegral $ length $ dict
+  let dictLen = fromIntegral $ length $ dict 
   let entropy = (log(dictLen) / log(2)) ^ (len)
-  -- ruby "join" 
-  mapM_ (>>= putStr . (++" ")) ((pick len dict))
-  putStrLn ""
+  let pws = foldr (addMon) (return "") $ pick len dict
+  pws >>= putStrLn 
   putStrLn $  "entropy is " ++ show entropy
