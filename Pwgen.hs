cda198bfc5c1282d8b45acac906a9e098ca11fb9
meris <meris.bahti@gmail.com>
2014-12-25 15:59:39 +0100

    Print out proper entropy calculations, and change it to use the proper calculations aswell.

--- Pwgen.hs
+++ Pwgen.hs
@@ -2,10 +2,11 @@ import System.Directory
 import System.Random (randomRIO)
 import Control.Applicative
 import Control.Monad
+import Data.Char
  
-upper  = 10
+upper  = 20
 lower  = 5 
-len    = 3 
+len    = 5 
 
 pick :: Int -> [a] -> [IO a]
 pick 0 xs = []
@@ -23,8 +24,9 @@ files = do
 main = do
   x <- files >>= mapM readFile 
   let dict = filter (liftA2 (&&) ((< upper).length) ((> lower).length)) (x >>= words)
-  let dictLen = fromIntegral $ length $ dict 
-  let entropy = (log(dictLen) / log(2)) ^ (len)
-  let pws = foldr1 (liftM2 (flip (++) . (' ':))) $ pick len dict
-  pws >>= putStrLn 
-  putStrLn $ "entropy is " ++ show entropy
+  let dictEntropy = log((fromIntegral $ length $ dict)^len) / log(2)
+  pws <- foldr1 (liftM2 (flip (++) . (' ':))) $ pick len dict
+  let letterEntropy = log(26^(fromIntegral $ length $ pws)) / log(2)
+  putStrLn . map toLower $ pws
+  putStrLn $ "[/usr/share/dict] entropy : " ++ show dictEntropy
+  putStrLn $ "[a-z] alphabet-entropy    : " ++ show letterEntropy 
