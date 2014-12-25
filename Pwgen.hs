935ba3181d5979e476ad3eacea98590c9a0d3ea7
meris <meris.bahti@gmail.com>
2014-12-25 16:46:12 +0100

    refactor so that the main loop handles less logic

--- Pwgen.hs
+++ Pwgen.hs
@@ -15,18 +15,24 @@ pick i xs = (pick' xs) : (pick (i-1) xs)
     pick' :: [a] -> IO a
     pick' xs = randomRIO (0, length xs - 1) >>= return . (xs !!)
  
+files :: IO [String]
 files = do 
   let dict = "/usr/share/dict/"
   fs <- getDirectoryContents dict
   return . (fmap (dict++)) . filter (liftA2 (&&) (/=".") (/="..")) $ fs
 
+genPw :: Int -> Int -> Int -> IO (String, Double, Double)
+genPw lowerWordLength upperWordLength len = do
+  x <- files >>= mapM readFile 
+  let dict = filter (liftA2 (&&) ((<= upperWordLength).length) ((>= lowerWordLength).length)) (x >>= words)
+  let dictEntropy = log (((^) . fromIntegral . length) dict len) / log 2
+  pws <- foldr1 (liftM2 (flip (++) . (' ':))) $ pick len dict
+  let letterEntropy = (/(log 2)) . log . (^) 26 . fromIntegral . length $ pws
+  return (map toLower $ pws, dictEntropy, letterEntropy)
 
 main = do
-  x <- files >>= mapM readFile 
-  let dict = filter (liftA2 (&&) ((< upper).length) ((> lower).length)) (x >>= words)
-  let dictEntropy = log((fromIntegral $ length $ dict)^len) / log(2)
-  pws <- foldr1 (liftM2 (flip (++) . (' ':))) $ pick len dict
-  let letterEntropy = log(26^(fromIntegral $ length $ pws)) / log(2)
-  putStrLn . map toLower $ pws
-  putStrLn $ "[/usr/share/dict] entropy : " ++ show dictEntropy
-  putStrLn $ "[a-z] alphabet-entropy    : " ++ show letterEntropy 
+  (pw, dictEntropy, letterEntropy) <- genPw 1 10 5
+  putStrLn pw
+  putStrLn $ "dictEntropy  : " ++ (show dictEntropy)
+  putStrLn $ "letterEntropy: " ++ (show letterEntropy)
+  return ()
