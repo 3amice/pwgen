8c875fd2c3897eca81722592ea8a608b7d106e7f
meris <meris.bahti@gmail.com>
2014-12-26 09:25:43 +0100

    add argument handler (and new main)

--- Pwgen.hs
+++ Pwgen.hs
@@ -1,33 +1,52 @@
-module Pwgen (genPw) where  
+import System.Console.GetOpt
+import System.Environment
+import Data.Maybe ( fromMaybe )
+import Text.Read
+import Utils 
 
-import System.Directory
-import System.Random (randomRIO)
-import Control.Applicative
-import Control.Monad
-import Data.Char
- 
-upper  = 20
-lower  = 5 
-len    = 5 
+data Flag 
+  = MinLength (Maybe Int) | MaxLength (Maybe Int) | NrWords (Maybe Int)
+    deriving Show
 
-pick :: Int -> [a] -> [IO a]
-pick 0 xs = []
-pick i xs = (pick' xs) : (pick (i-1) xs)
-  where
-    pick' :: [a] -> IO a
-    pick' xs = randomRIO (0, length xs - 1) >>= return . (xs !!)
- 
-files :: IO [String]
-files = do 
-  let dict = "/usr/share/dict/"
-  fs <- getDirectoryContents dict
-  return . (fmap (dict++)) . filter (liftA2 (&&) (/=".") (/="..")) $ fs
+options :: [OptDescr Flag]
+options =
+  [ Option []     ["minlength"]  (ReqArg (MinLength . readMaybe)  "integer") "minimum length of words from dictionary (default 5)"
+  , Option []     ["maxlength"]  (ReqArg (MaxLength . readMaybe)  "integer") "maximum length of words from dictionary (default 20)"
+  , Option ['l']     ["words"]      (ReqArg (NrWords . readMaybe)  "integer") "number of words to use (default 4)"
+  ]
 
-genPw :: Int -> Int -> Int -> IO (String, Double, Double)
-genPw lowerWordLength upperWordLength len = do
-  x <- files >>= mapM readFile 
-  let dict = filter (liftA2 (&&) ((<= upperWordLength).length) ((>= lowerWordLength).length)) (x >>= words)
-  let dictEntropy = log (((^) . fromIntegral . length) dict len) / log 2
-  pws <- foldr1 (liftM2 (flip (++) . (' ':))) $ pick len dict
-  let letterEntropy = (/(log 2)) . log . (^) 26 . fromIntegral . length $ pws
-  return (map toLower $ pws, dictEntropy, letterEntropy)
+compilerOpts :: [String] -> IO ([Flag], [String])
+compilerOpts argv = 
+  case getOpt Permute options argv of
+     (o,n,[])   -> return (o,n)
+     (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+ where header = "Usage: Pwgen [OPTION...] files..."
+
+injectOpts :: [Flag] -> IO (String, Double, Double)
+injectOpts flags = do
+  let maxLength = findMaxLength flags
+  let minLength = findMinLength flags
+  let nrWords   = findWords flags
+  genPw minLength maxLength nrWords
+    where
+      findMinLength :: [Flag] -> Int 
+      findMinLength []           = 5 
+      findMinLength ((MinLength (Just a)): _) = a
+      findMinLength (_:xs) = findMinLength xs
+
+      findMaxLength :: [Flag] -> Int 
+      findMaxLength []           = 20
+      findMaxLength ((MaxLength (Just a)): _) = a
+      findMaxLength (_:xs) = findMaxLength xs
+
+      findWords     :: [Flag] -> Int 
+      findWords     []           = 4 
+      findWords     ((NrWords (Just a)): _) = a
+      findWords     (_:xs) = findWords xs
+
+
+main = do
+  args <- getArgs
+  (flags, _) <- compilerOpts args
+  result <- injectOpts flags
+  putStrLn $ show $ result 
---
+++ utils.hs
@@ -0,0 +1,33 @@
+module Utils (genPw) where  
+
+import System.Directory
+import System.Random (randomRIO)
+import Control.Applicative
+import Control.Monad
+import Data.Char
+ 
+upper  = 20
+lower  = 5 
+len    = 5 
+
+pick :: Int -> [a] -> [IO a]
+pick 0 xs = []
+pick i xs = (pick' xs) : (pick (i-1) xs)
+  where
+    pick' :: [a] -> IO a
+    pick' xs = randomRIO (0, length xs - 1) >>= return . (xs !!)
+ 
+files :: IO [String]
+files = do 
+  let dict = "/usr/share/dict/"
+  fs <- getDirectoryContents dict
+  return . (fmap (dict++)) . filter (liftA2 (&&) (/=".") (/="..")) $ fs
+
+genPw :: Int -> Int -> Int -> IO (String, Double, Double)
+genPw lowerWordLength upperWordLength len = do
+  x <- files >>= mapM readFile 
+  let dict = filter (liftA2 (&&) ((<= upperWordLength).length) ((>= lowerWordLength).length)) (x >>= words)
+  let dictEntropy = log (((^) . fromIntegral . length) dict len) / log 2
+  pws <- foldr1 (liftM2 (flip (++) . (' ':))) $ pick len dict
+  let letterEntropy = (/(log 2)) . log . (^) 26 . fromIntegral . length $ pws
+  return (map toLower $ pws, dictEntropy, letterEntropy)
